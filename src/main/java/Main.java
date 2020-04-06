20f66980532995229ffbc6086fba5d90041e00f7
Bulat Khabibullin <bulat22101@yahoo.com>
2020-04-06 06:38:30 +0500

    SMH - 3
    Make code simple and fix deletion bug
    -remove useless exceptions
    -remove useless entities

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
-import enity.MigrationReport;
 import manager.MigrationManager;
 
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,7 +10,7 @@ public class Main {
     private static final String SOURCE_STORAGE_URL_SUFFIX = "/oldStorage";
     private static final String TARGET_STORAGE_URL_SUFFIX = "/newStorage";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         MigrationManager migrationManager = new MigrationManager();
         Scanner input = new Scanner(System.in);
         PrintWriter output = new PrintWriter(System.out);
@@ -19,7 +20,7 @@ public class Main {
         output.printf("Please enter target storage url. Default: %s.\n", DEFAULT_URL);
         output.flush();
         String readTargetStorageUrl = input.nextLine();
-        output.printf("Overwrite files that are already in target storage? (y, n) Default: n.\n");
+        output.println("Overwrite files that are already in target storage? (y, n) Default: n.");
         output.flush();
         String readOverwrite = input.nextLine();
         String sourceStorageUrl = (readSourceStorageUrl.isEmpty() ? DEFAULT_URL : readSourceStorageUrl)
@@ -27,13 +28,10 @@ public class Main {
         String targetStorageUrl = (readTargetStorageUrl.isEmpty() ? DEFAULT_URL : readTargetStorageUrl)
                 + TARGET_STORAGE_URL_SUFFIX;
         boolean overwrite = !readOverwrite.isEmpty() && Character.toLowerCase(readOverwrite.charAt(0)) == 'y';
-        MigrationReport migrationReport = migrationManager
-                .proceedMigration(sourceStorageUrl, targetStorageUrl, overwrite);
-        output.println("Migration Report.");
-        output.printf("Verdict: %s.\n", migrationReport.isMigrationDone() ? "DONE" : "FAILED");
-        for (String message : migrationReport.getMessages()) {
-            output.println(message);
-        }
+        String result = migrationManager.proceedMigration(sourceStorageUrl, targetStorageUrl, overwrite)
+                ? "done"
+                : "failed";
+        output.printf("Migration %s.", result);
         output.println("Press ENTER to quit.");
         output.flush();
         input.nextLine();
--- src/main/java/manager/MigrationManager.java
+++ src/main/java/manager/MigrationManager.java
@@ -1,9 +1,6 @@
 package manager;
 
 import connector.FaultyStorageConnector;
-import enity.MigrationReport;
-import enity.MigrationSets;
-import exception.MigrationException;
 import util.retry.RetryService;
 
 import java.util.*;
@@ -15,143 +12,75 @@ public class MigrationManager {
         this.retryService = new RetryService();
     }
 
-    public MigrationReport proceedMigration(String sourceStorageUrl, String targetStorageUrl, boolean overwrite) {
-        List<String> reportMessages = new ArrayList<>();
+    public boolean proceedMigration(String sourceStorageUrl, String targetStorageUrl, boolean overwrite) {
         FaultyStorageConnector sourceStorage = new FaultyStorageConnector(sourceStorageUrl);
         FaultyStorageConnector targetStorage = new FaultyStorageConnector(targetStorageUrl);
-        Set<String> filesToMove;
+        Optional<List<String>> sourceStorageFiles = getStorageFiles(sourceStorage);
+        Optional<List<String>> targetStorageFiles = getStorageFiles(targetStorage);
+        if (!sourceStorageFiles.isPresent() || !targetStorageFiles.isPresent()) {
+            return false;
+        }
+        Set<String> filesToMove = new HashSet<>(sourceStorageFiles.get());
         Set<String> filesToOverwrite;
-        try {
-            MigrationSets migrationSets = getMigrationSets(sourceStorage, targetStorage, overwrite);
-            filesToMove = migrationSets.getFilesToMove();
-            filesToOverwrite = migrationSets.getFilesToOverwrite();
-            String message = String.format(
-                    "Successfully get actual file lists in storages. Files to move: %d. Will be overwritten: %d.",
-                    filesToMove.size(),
-                    filesToOverwrite.size()
-            );
-            reportMessages.add(message);
-        } catch (MigrationException e) {
-            reportMessages.add("Error while getting file lists: " + e.getMessage());
-            return new MigrationReport(false, reportMessages);
-        }
-        boolean allFilesCopied = true;
-        for (String filename : filesToMove) {
-            try {
-                copyFile(sourceStorage, targetStorage, filename, filesToOverwrite.contains(filename));
-            } catch (MigrationException e) {
-                allFilesCopied = false;
-                String message = String.format(
-                        "Error while copying file %s: %s",
-                        filename,
-                        e.getMessage()
-                );
-                reportMessages.add(message);
-            }
-        }
-        if (allFilesCopied) {
-            reportMessages.add("All files were successfully copied");
-        }
-        boolean allFilesChecked;
-        try {
-            allFilesChecked = checkAllFilesMoved(sourceStorage, targetStorage);
-        } catch (MigrationException e) {
-            allFilesChecked = false;
-            String message = ("Error while checking resulting file lists: " + e.getMessage());
-            reportMessages.add(message);
-        }
-        if (allFilesChecked) {
-            reportMessages.add("All files are in target storage file list.");
-        } else {
-            reportMessages.add("Some files are missing in target storage file list.");
-        }
-        boolean allFilesDeleted = true;
-        if (allFilesChecked) {
-            for (String filename : filesToMove) {
-                try {
-                    deleteFile(sourceStorage, filename);
-                } catch (MigrationException e) {
-                    allFilesDeleted = false;
-                    String message = String.format(
-                            "Error while deleting file %s: %s",
-                            filename,
-                            e.getMessage()
-                    );
-                    reportMessages.add(message);
-                }
-            }
-        } else {
-            allFilesDeleted = false;
-        }
-        if(allFilesDeleted){
-            reportMessages.add("All files were successfully deleted from source storage");
-        }
-        return new MigrationReport(allFilesCopied && allFilesChecked && allFilesDeleted, reportMessages);
-    }
-
-
-    private boolean checkAllFilesMoved(FaultyStorageConnector sourceStorage, FaultyStorageConnector targetStorage)
-            throws MigrationException {
-        return getMigrationSets(sourceStorage, targetStorage, false).getFilesToMove().isEmpty();
-    }
-
-    private MigrationSets getMigrationSets(FaultyStorageConnector sourceStorage,
-                                           FaultyStorageConnector targetStorage,
-                                           boolean overwrite)
-            throws MigrationException {
-        Set<String> filesToMove = new HashSet<>(getStorageFiles(sourceStorage));
-        Set<String> targetStorageFiles = new HashSet<>(getStorageFiles(targetStorage));
-        Set<String> filesToOverwrite;
-        if (!overwrite) {
-            filesToMove.removeAll(targetStorageFiles);
-            filesToOverwrite = Collections.emptySet();
-        } else {
-            filesToOverwrite = new HashSet<>(filesToMove);
-            filesToOverwrite.retainAll(targetStorageFiles);
-        }
-        return new MigrationSets(filesToMove, filesToOverwrite);
-    }
-
-    private void copyFile(FaultyStorageConnector sourceStorage, FaultyStorageConnector targetStorage,
-                          String filename, boolean overwrite) throws MigrationException {
-        byte[] file = downloadFile(sourceStorage, filename);
         if (overwrite) {
-            deleteFile(targetStorage, filename);
+            filesToOverwrite = new HashSet<>(filesToMove);
+            filesToOverwrite.retainAll(new HashSet<>(targetStorageFiles.get()));
+        } else {
+            filesToOverwrite = Collections.emptySet();
+            filesToMove.removeAll(new HashSet<>(targetStorageFiles.get()));
         }
-        uploadFile(targetStorage, filename, file);
+        return copyAllFiles(sourceStorage, targetStorage, filesToMove, filesToOverwrite)
+                && checkFiles(sourceStorageFiles.get(), targetStorage)
+                && deleteAllFiles(sourceStorage, sourceStorageFiles.get());
     }
 
-    private List<String> getStorageFiles(FaultyStorageConnector storage) throws MigrationException {
-        return retryService.retry(storage::getFileNamesList)
-                .orElseThrow(() -> new MigrationException(
-                        String.format("Can't reach storage: %s.", storage.getBaseUrl())
-                ));
+    private boolean checkFiles(Collection<String> expectedFiles, FaultyStorageConnector storage){
+        Optional<Set<String>> storageFiles = getStorageFiles(storage).map(HashSet::new);
+        return storageFiles.isPresent() && storageFiles.get().containsAll(expectedFiles);
     }
 
-    private void deleteFile(FaultyStorageConnector storage, String filename) throws MigrationException {
-        retryService.retry(() -> Optional.of(storage.deleteFile(filename)), value -> value)
-                .orElseThrow(
-                        () -> new MigrationException(
-                                String.format("Can't delete file %s in storage %s.", filename, storage.getBaseUrl())
-                        )
-                );
+    private boolean deleteAllFiles(FaultyStorageConnector storage, Collection<String> files) {
+        System.err.println("START DELETION.");
+        boolean allFilesDeleted = true;
+        for (String filename : files) {
+            allFilesDeleted &= deleteFile(storage, filename);
+        }
+        return allFilesDeleted;
     }
 
-    private byte[] downloadFile(FaultyStorageConnector storage, String filename) throws MigrationException {
-        return retryService.retry(() -> storage.downloadFile(filename))
-                .orElseThrow(
-                        () -> new MigrationException(
-                                String.format("Can't download file %s from storage %s.", filename, storage.getBaseUrl())
-                        )
-                );
+    private boolean copyAllFiles(FaultyStorageConnector sourceStorage, FaultyStorageConnector targetStorage,
+                                 Collection<String> filesToCopy, Set<String> filesToOverwrite) {
+        System.err.println("START COPYING.");
+        boolean allFilesCopied = true;
+        for (String filename : filesToCopy) {
+            allFilesCopied &= copyFile(sourceStorage, targetStorage, filename, filesToOverwrite.contains(filename));
+        }
+        return allFilesCopied;
     }
 
-    private void uploadFile(FaultyStorageConnector storage, String filename, byte[] file) throws MigrationException {
-        retryService.retry(() -> Optional.of(storage.uploadFile(filename, file)), value -> value)
-                .orElseThrow(
-                        () -> new MigrationException(
-                                String.format("Can't upload file %s to storage %s.", filename, storage.getBaseUrl())
-                        )
-                );
+    private boolean copyFile(FaultyStorageConnector sourceStorage, FaultyStorageConnector targetStorage,
+                             String filename, boolean overwrite) {
+        System.err.println(filename);
+        Optional<byte[]> file = downloadFile(sourceStorage, filename);
+        return file.isPresent()
+                && (!overwrite || deleteFile(targetStorage, filename))
+                && uploadFile(targetStorage, filename, file.get());
+    }
+
+    private Optional<List<String>> getStorageFiles(FaultyStorageConnector storage) {
+        return retryService.retry(storage::getFileNamesList);
+    }
+
+    private boolean deleteFile(FaultyStorageConnector storage, String filename) {
+        System.err.println(filename);
+        return retryService.retry(() -> Optional.of(storage.deleteFile(filename)), value -> value).isPresent();
+    }
+
+    private Optional<byte[]> downloadFile(FaultyStorageConnector storage, String filename) {
+        return retryService.retry(() -> storage.downloadFile(filename));
+    }
+
+    private boolean uploadFile(FaultyStorageConnector storage, String filename, byte[] file) {
+        return retryService.retry(() -> Optional.of(storage.uploadFile(filename, file)), value -> value).isPresent();
     }
 }
